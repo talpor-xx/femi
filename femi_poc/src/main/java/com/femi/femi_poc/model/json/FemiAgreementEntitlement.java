95e8a0d8383f2aa0773f4012dc1640e1862482bf
brms <>
2018-09-05 17:14:28 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementEntitlement.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementEntitlement.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementEntitlement.java
@@ -1,37 +1,18 @@
 package com.femi.femi_poc.model.json;
 
 import java.io.Serializable;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
 import java.util.Date;
 
 public class FemiAgreementEntitlement implements Serializable
 {
 
-   @com.google.gson.annotations.SerializedName("Id")
-   @com.google.gson.annotations.Expose
    private String id;
-   @com.google.gson.annotations.SerializedName("Expose")
-   @com.google.gson.annotations.Expose
    private String expose;
-   @com.google.gson.annotations.SerializedName("MaximumServices")
-   @com.google.gson.annotations.Expose
    private String maximumServices;
-   @com.google.gson.annotations.SerializedName("Name4")
-   @com.google.gson.annotations.Expose
    private String name4;
-   @com.google.gson.annotations.SerializedName("QualificationDate2")
-   @com.google.gson.annotations.Expose
-   private Date qualificationDate2;
-   @com.google.gson.annotations.SerializedName("SumServicesUsage")
-   @com.google.gson.annotations.Expose
    private String sumServicesUsage;
-   @com.google.gson.annotations.SerializedName("ListOfFemiAgreementLineItemAsset")
-   @com.google.gson.annotations.Expose
    private ListOfFemiAgreementLineItemAsset listOfFemiAgreementLineItemAsset;
    private final static long serialVersionUID = 7862339401013262127L;
-
-   private java.util.Date currentDate;
 
    public String getId()
    {
@@ -73,16 +54,6 @@ public class FemiAgreementEntitlement implements Serializable
       this.name4 = name4;
    }
 
-   public Date getQualificationDate2()
-   {
-      return qualificationDate2;
-   }
-
-   public void setQualificationDate2(Date qualificationDate2)
-   {
-      this.qualificationDate2 = qualificationDate2;
-   }
-
    public String getSumServicesUsage()
    {
       return sumServicesUsage;
@@ -109,7 +80,6 @@ public class FemiAgreementEntitlement implements Serializable
    {
       return "FemiAgreementEntitlement [id=" + id + ", expose=" + expose
             + ", maximumServices=" + maximumServices + ", name4=" + name4
-            + ", qualificationDate2=" + qualificationDate2
             + ", sumServicesUsage=" + sumServicesUsage
             + ", listOfFemiAgreementLineItemAsset="
             + listOfFemiAgreementLineItemAsset + "]";
@@ -119,34 +89,20 @@ public class FemiAgreementEntitlement implements Serializable
    {
    }
 
-   public java.util.Date getCurrentDate()
-   {
-      return this.currentDate;
-   }
-
-   public void setCurrentDate(java.util.Date currentDate)
-   {
-      this.currentDate = currentDate;
-   }
-
    public FemiAgreementEntitlement(
          java.lang.String id,
          java.lang.String expose,
          java.lang.String maximumServices,
          java.lang.String name4,
-         java.util.Date qualificationDate2,
          java.lang.String sumServicesUsage,
-         com.femi.femi_poc.model.json.ListOfFemiAgreementLineItemAsset listOfFemiAgreementLineItemAsset,
-         java.util.Date currentDate)
+         com.femi.femi_poc.model.json.ListOfFemiAgreementLineItemAsset listOfFemiAgreementLineItemAsset)
    {
       this.id = id;
       this.expose = expose;
       this.maximumServices = maximumServices;
       this.name4 = name4;
-      this.qualificationDate2 = qualificationDate2;
       this.sumServicesUsage = sumServicesUsage;
       this.listOfFemiAgreementLineItemAsset = listOfFemiAgreementLineItemAsset;
-      this.currentDate = currentDate;
    }
 
 }
