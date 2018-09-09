17c5cedd3ba5ae60fdde84b495cfc6332092ef49
brms <>
2018-09-09 16:11:19 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/Contacts.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/Contacts.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/Contacts.java
@@ -1,25 +1,33 @@
 
 package com.femi.femi_poc.model.json;
 
+
 import java.io.Serializable;
 import java.util.List;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "Contact"
+})
 public class Contacts implements Serializable
 {
 
-    @SerializedName("Contact")
-    @Expose
+    @JsonProperty("Contact")
     private List<Contact> contact = null;
     private final static long serialVersionUID = 5920695057585869143L;
 
+    @JsonProperty("Contact")
     public List<Contact> getContact() {
         return contact;
     }
 
+    @JsonProperty("Contact")
     public void setContact(List<Contact> contact) {
         this.contact = contact;
     }
 
 }
+
