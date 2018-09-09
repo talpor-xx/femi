aa3fa5cf6aa44f3e93574a48f319d58c0aebf4e7
brms <>
2018-09-09 16:16:17 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/FEMIFSAgreementSubItem.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/FEMIFSAgreementSubItem.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/FEMIFSAgreementSubItem.java
@@ -2,87 +2,105 @@
 package com.femi.femi_poc.model.json;
 
 import java.io.Serializable;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "Id",
+    "ParentAgreementItemId",
+    "ProductId",
+    "LineNumber",
+    "Product",
+    "Write-InProduct",
+    "LineNumber2"
+})
 public class FEMIFSAgreementSubItem implements Serializable
 {
 
-    @SerializedName("Id")
-    @Expose
+    @JsonProperty("Id")
     private String id;
-    @SerializedName("ParentAgreementItemId")
-    @Expose
+    @JsonProperty("ParentAgreementItemId")
     private String parentAgreementItemId;
-    @SerializedName("ProductId")
-    @Expose
+    @JsonProperty("ProductId")
     private String productId;
-    @SerializedName("LineNumber")
-    @Expose
+    @JsonProperty("LineNumber")
     private String lineNumber;
-    @SerializedName("Product")
-    @Expose
+    @JsonProperty("Product")
     private String product;
-    @SerializedName("Write-InProduct")
-    @Expose
+    @JsonProperty("Write-InProduct")
     private String writeInProduct;
-    @SerializedName("LineNumber2")
-    @Expose
+    @JsonProperty("LineNumber2")
     private String lineNumber2;
     private final static long serialVersionUID = 6411221798360685909L;
 
+    @JsonProperty("Id")
     public String getId() {
         return id;
     }
 
+    @JsonProperty("Id")
     public void setId(String id) {
         this.id = id;
     }
 
+    @JsonProperty("ParentAgreementItemId")
     public String getParentAgreementItemId() {
         return parentAgreementItemId;
     }
 
+    @JsonProperty("ParentAgreementItemId")
     public void setParentAgreementItemId(String parentAgreementItemId) {
         this.parentAgreementItemId = parentAgreementItemId;
     }
 
+    @JsonProperty("ProductId")
     public String getProductId() {
         return productId;
     }
 
+    @JsonProperty("ProductId")
     public void setProductId(String productId) {
         this.productId = productId;
     }
 
+    @JsonProperty("LineNumber")
     public String getLineNumber() {
         return lineNumber;
     }
 
+    @JsonProperty("LineNumber")
     public void setLineNumber(String lineNumber) {
         this.lineNumber = lineNumber;
     }
 
+    @JsonProperty("Product")
     public String getProduct() {
         return product;
     }
 
+    @JsonProperty("Product")
     public void setProduct(String product) {
         this.product = product;
     }
 
+    @JsonProperty("Write-InProduct")
     public String getWriteInProduct() {
         return writeInProduct;
     }
 
+    @JsonProperty("Write-InProduct")
     public void setWriteInProduct(String writeInProduct) {
         this.writeInProduct = writeInProduct;
     }
 
+    @JsonProperty("LineNumber2")
     public String getLineNumber2() {
         return lineNumber2;
     }
 
+    @JsonProperty("LineNumber2")
     public void setLineNumber2(String lineNumber2) {
         this.lineNumber2 = lineNumber2;
     }
