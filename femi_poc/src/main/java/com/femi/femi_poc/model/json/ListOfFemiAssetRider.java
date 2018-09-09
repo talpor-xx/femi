d66908ee5995dd904a231995874fc58fc6650d4b
brms <>
2018-09-09 16:17:39 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAssetRider.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAssetRider.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAssetRider.java
@@ -2,21 +2,27 @@
 package com.femi.femi_poc.model.json;
 
 import java.io.Serializable;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "FemiAsset-Rider"
+})
 public class ListOfFemiAssetRider implements Serializable
 {
 
-    @SerializedName("FemiAsset-Rider")
-    @Expose
+    @JsonProperty("FemiAsset-Rider")
     private FemiAssetRider femiAssetRider;
     private final static long serialVersionUID = -4414195727471244033L;
 
+    @JsonProperty("FemiAsset-Rider")
     public FemiAssetRider getFemiAssetRider() {
         return femiAssetRider;
     }
 
+    @JsonProperty("FemiAsset-Rider")
     public void setFemiAssetRider(FemiAssetRider femiAssetRider) {
         this.femiAssetRider = femiAssetRider;
     }
