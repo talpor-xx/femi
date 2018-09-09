40400b0ee5f485f266eee28b5b324c1b2ef39187
brms <>
2018-09-09 16:17:06 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAgreementLineItemAsset.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAgreementLineItemAsset.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAgreementLineItemAsset.java
@@ -3,21 +3,27 @@ package com.femi.femi_poc.model.json;
 
 import java.io.Serializable;
 import java.util.List;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "FemiAgreementLineItemAsset"
+})
 public class ListOfFemiAgreementLineItemAsset implements Serializable
 {
 
-    @SerializedName("FemiAgreementLineItemAsset")
-    @Expose
+    @JsonProperty("FemiAgreementLineItemAsset")
     private List<FemiAgreementLineItemAsset> femiAgreementLineItemAsset = null;
     private final static long serialVersionUID = 7966369851638266882L;
 
+    @JsonProperty("FemiAgreementLineItemAsset")
     public List<FemiAgreementLineItemAsset> getFemiAgreementLineItemAsset() {
         return femiAgreementLineItemAsset;
     }
 
+    @JsonProperty("FemiAgreementLineItemAsset")
     public void setFemiAgreementLineItemAsset(List<FemiAgreementLineItemAsset> femiAgreementLineItemAsset) {
         this.femiAgreementLineItemAsset = femiAgreementLineItemAsset;
     }
