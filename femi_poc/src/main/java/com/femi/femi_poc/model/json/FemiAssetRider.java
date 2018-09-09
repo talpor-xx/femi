684fb0b7d94996fa714063e76c18eb4f78186fff
brms <>
2018-09-09 16:15:57 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAssetRider.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAssetRider.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAssetRider.java
@@ -1,99 +1,120 @@
 
 package com.femi.femi_poc.model.json;
 
-import java.io.Serializable;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "Id",
+    "AccountName",
+    "OwnerAccountId",
+    "PolicySourceId",
+    "PrimaryContactId2",
+    "ProductName",
+    "StartDate",
+    "ListOfFemiAgreementEntitlement"
+})
 public class FemiAssetRider implements Serializable
 {
 
-    @SerializedName("Id")
-    @Expose
+    @JsonProperty("Id")
     private String id;
-    @SerializedName("AccountName")
-    @Expose
+    @JsonProperty("AccountName")
     private String accountName;
-    @SerializedName("OwnerAccountId")
-    @Expose
+    @JsonProperty("OwnerAccountId")
     private String ownerAccountId;
-    @SerializedName("PolicySourceId")
-    @Expose
+    @JsonProperty("PolicySourceId")
     private String policySourceId;
-    @SerializedName("PrimaryContactId2")
-    @Expose
+    @JsonProperty("PrimaryContactId2")
     private String primaryContactId2;
-    @SerializedName("ProductName")
-    @Expose
+    @JsonProperty("ProductName")
     private String productName;
-    @SerializedName("StartDate")
-    @Expose
+    @JsonProperty("StartDate")
     private String startDate;
-    @SerializedName("ListOfFemiAgreementEntitlement")
-    @Expose
+    @JsonProperty("ListOfFemiAgreementEntitlement")
     private ListOfFemiAgreementEntitlement listOfFemiAgreementEntitlement;
     private final static long serialVersionUID = -2624027829571265715L;
 
+    @JsonProperty("Id")
     public String getId() {
         return id;
     }
 
+    @JsonProperty("Id")
     public void setId(String id) {
         this.id = id;
     }
 
+    @JsonProperty("AccountName")
     public String getAccountName() {
         return accountName;
     }
 
+    @JsonProperty("AccountName")
     public void setAccountName(String accountName) {
         this.accountName = accountName;
     }
 
+    @JsonProperty("OwnerAccountId")
     public String getOwnerAccountId() {
         return ownerAccountId;
     }
 
+    @JsonProperty("OwnerAccountId")
     public void setOwnerAccountId(String ownerAccountId) {
         this.ownerAccountId = ownerAccountId;
     }
 
+    @JsonProperty("PolicySourceId")
     public String getPolicySourceId() {
         return policySourceId;
     }
 
+    @JsonProperty("PolicySourceId")
     public void setPolicySourceId(String policySourceId) {
         this.policySourceId = policySourceId;
     }
 
+    @JsonProperty("PrimaryContactId2")
     public String getPrimaryContactId2() {
         return primaryContactId2;
     }
 
+    @JsonProperty("PrimaryContactId2")
     public void setPrimaryContactId2(String primaryContactId2) {
         this.primaryContactId2 = primaryContactId2;
     }
 
+    @JsonProperty("ProductName")
     public String getProductName() {
         return productName;
     }
 
+    @JsonProperty("ProductName")
     public void setProductName(String productName) {
         this.productName = productName;
     }
 
+    @JsonProperty("StartDate")
     public String getStartDate() {
         return startDate;
     }
 
+    @JsonProperty("StartDate")
     public void setStartDate(String startDate) {
         this.startDate = startDate;
     }
 
+    @JsonProperty("ListOfFemiAgreementEntitlement")
     public ListOfFemiAgreementEntitlement getListOfFemiAgreementEntitlement() {
         return listOfFemiAgreementEntitlement;
     }
 
+    @JsonProperty("ListOfFemiAgreementEntitlement")
     public void setListOfFemiAgreementEntitlement(ListOfFemiAgreementEntitlement listOfFemiAgreementEntitlement) {
         this.listOfFemiAgreementEntitlement = listOfFemiAgreementEntitlement;
     }
