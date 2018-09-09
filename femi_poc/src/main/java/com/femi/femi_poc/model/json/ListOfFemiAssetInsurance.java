588184cc8c8ca097a0b0fcd6f9ca9563029da320
brms <>
2018-09-09 16:17:22 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAssetInsurance.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAssetInsurance.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAssetInsurance.java
@@ -2,21 +2,27 @@
 package com.femi.femi_poc.model.json;
 
 import java.io.Serializable;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "FemiAsset-Insurance"
+})
 public class ListOfFemiAssetInsurance implements Serializable
 {
 
-    @SerializedName("FemiAsset-Insurance")
-    @Expose
+    @JsonProperty("FemiAsset-Insurance")
     private FemiAssetInsurance femiAssetInsurance;
     private final static long serialVersionUID = 1404160448797383814L;
 
+    @JsonProperty("FemiAsset-Insurance")
     public FemiAssetInsurance getFemiAssetInsurance() {
         return femiAssetInsurance;
     }
 
+    @JsonProperty("FemiAsset-Insurance")
     public void setFemiAssetInsurance(FemiAssetInsurance femiAssetInsurance) {
         this.femiAssetInsurance = femiAssetInsurance;
     }
