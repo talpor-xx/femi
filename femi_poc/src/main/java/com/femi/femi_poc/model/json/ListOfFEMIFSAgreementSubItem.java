d9cedd5e2d126a53cd74030c643109bdf6116b3d
brms <>
2018-09-09 16:17:59 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFEMIFSAgreementSubItem.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFEMIFSAgreementSubItem.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFEMIFSAgreementSubItem.java
@@ -3,21 +3,27 @@ package com.femi.femi_poc.model.json;
 
 import java.io.Serializable;
 import java.util.List;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "FEMIFSAgreementSubItem"
+})
 public class ListOfFEMIFSAgreementSubItem implements Serializable
 {
 
-    @SerializedName("FEMIFSAgreementSubItem")
-    @Expose
+    @JsonProperty("FEMIFSAgreementSubItem")
     private List<FEMIFSAgreementSubItem> fEMIFSAgreementSubItem = null;
     private final static long serialVersionUID = 6435530656760871593L;
 
+    @JsonProperty("FEMIFSAgreementSubItem")
     public List<FEMIFSAgreementSubItem> getFEMIFSAgreementSubItem() {
         return fEMIFSAgreementSubItem;
     }
 
+    @JsonProperty("FEMIFSAgreementSubItem")
     public void setFEMIFSAgreementSubItem(List<FEMIFSAgreementSubItem> fEMIFSAgreementSubItem) {
         this.fEMIFSAgreementSubItem = fEMIFSAgreementSubItem;
     }
