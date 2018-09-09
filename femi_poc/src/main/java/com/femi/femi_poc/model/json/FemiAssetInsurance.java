f016ea57798fe0ab734ba14c2036b185178ef558
brms <>
2018-09-09 16:15:38 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAssetInsurance.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAssetInsurance.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAssetInsurance.java
@@ -1,55 +1,66 @@
 
 package com.femi.femi_poc.model.json;
-
 import java.io.Serializable;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "Id",
+    "ProductName",
+    "SerialNumber",
+    "ListOfFemiAsset-Rider"
+})
 public class FemiAssetInsurance implements Serializable
 {
 
-    @SerializedName("Id")
-    @Expose
+    @JsonProperty("Id")
     private String id;
-    @SerializedName("ProductName")
-    @Expose
+    @JsonProperty("ProductName")
     private String productName;
-    @SerializedName("SerialNumber")
-    @Expose
+    @JsonProperty("SerialNumber")
     private String serialNumber;
-    @SerializedName("ListOfFemiAsset-Rider")
-    @Expose
+    @JsonProperty("ListOfFemiAssetRider")
     private ListOfFemiAssetRider listOfFemiAssetRider;
     private final static long serialVersionUID = -5365466670916398959L;
 
+    @JsonProperty("Id")
     public String getId() {
         return id;
     }
 
+    @JsonProperty("Id")
     public void setId(String id) {
         this.id = id;
     }
 
+    @JsonProperty("ProductName")
     public String getProductName() {
         return productName;
     }
 
+    @JsonProperty("ProductName")
     public void setProductName(String productName) {
         this.productName = productName;
     }
 
+    @JsonProperty("SerialNumber")
     public String getSerialNumber() {
         return serialNumber;
     }
 
+    @JsonProperty("SerialNumber")
     public void setSerialNumber(String serialNumber) {
         this.serialNumber = serialNumber;
     }
 
+    @JsonProperty("ListOfFemiAsset-Rider")
     public ListOfFemiAssetRider getListOfFemiAssetRider() {
         return listOfFemiAssetRider;
     }
 
+    @JsonProperty("ListOfFemiAsset-Rider")
     public void setListOfFemiAssetRider(ListOfFemiAssetRider listOfFemiAssetRider) {
         this.listOfFemiAssetRider = listOfFemiAssetRider;
     }
