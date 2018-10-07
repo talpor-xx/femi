6f303d8eabeadcf288d9ea2518517cf353f764c2
brms <>
2018-10-07 20:05:35 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAgreementEntitlement.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAgreementEntitlement.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFemiAgreementEntitlement.java
@@ -1,8 +1,12 @@
 
 package com.femi.femi_poc.model.json;
 
-import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -11,12 +15,12 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonPropertyOrder({
     "FemiAgreementEntitlement"
 })
-public class ListOfFemiAgreementEntitlement implements Serializable
-{
+public class ListOfFemiAgreementEntitlement {
 
     @JsonProperty("FemiAgreementEntitlement")
     private List<FemiAgreementEntitlement> femiAgreementEntitlement = null;
-    private final static long serialVersionUID = -8710699408880458706L;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
     @JsonProperty("FemiAgreementEntitlement")
     public List<FemiAgreementEntitlement> getFemiAgreementEntitlement() {
@@ -26,6 +30,16 @@ public class ListOfFemiAgreementEntitlement implements Serializable
     @JsonProperty("FemiAgreementEntitlement")
     public void setFemiAgreementEntitlement(List<FemiAgreementEntitlement> femiAgreementEntitlement) {
         this.femiAgreementEntitlement = femiAgreementEntitlement;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
     }
 
 }
