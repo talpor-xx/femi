09d9363451dcfbd4cd0dd9336a61bac55d1dcab9
brms <>
2018-10-07 20:02:43 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementEntitlement.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementEntitlement.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementEntitlement.java
@@ -1,141 +1,139 @@
 package com.femi.femi_poc.model.json;
 
-import java.io.Serializable;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import java.util.Date;
-import java.util.List;
 
-@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
-@com.fasterxml.jackson.annotation.JsonPropertyOrder({ "Id", "Expose",
-      "MaximumServices", "Name4", "QualificationDate2", "SumServicesUsage",
-      "ListOfFemiAgreementLineItemAsset" })
-public class FemiAgreementEntitlement implements Serializable
-{
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "Id",
+    "Expose",
+    "MaximumServices",
+    "Name4",
+    "QualificationDate2",
+    "SumServicesUsage",
+    "ListOfFemiAgreementLineItemAsset"
+})
+public class FemiAgreementEntitlement {
 
-   @com.fasterxml.jackson.annotation.JsonProperty("Id")
-   private String id;
-   @com.fasterxml.jackson.annotation.JsonProperty("Expose")
-   private String expose;
-   @com.fasterxml.jackson.annotation.JsonProperty("MaximumServices")
-   private String maximumServices;
-   @com.fasterxml.jackson.annotation.JsonProperty("Name4")
-   private String name4;
-   @com.fasterxml.jackson.annotation.JsonProperty("QualificationDate2")
-   private Date qualificationDate2;
-   @com.fasterxml.jackson.annotation.JsonProperty("SumServicesUsage")
-   private String sumServicesUsage;
-   @com.fasterxml.jackson.annotation.JsonProperty("ListOfFemiAgreementLineItemAsset")
-   private ListOfFemiAgreementLineItemAsset[] listOfFemiAgreementLineItemAsset;
+    @JsonProperty("Id")
+    private String id;
+    @JsonProperty("Expose")
+    private String expose;
+    @JsonProperty("MaximumServices")
+    private String maximumServices;
+    @JsonProperty("Name4")
+    private String name4;
+    @JsonProperty("QualificationDate2")
+    private String qualificationDate2;
+    @JsonProperty("SumServicesUsage")
+    private String sumServicesUsage;
+    @JsonProperty("ListOfFemiAgreementLineItemAsset")
+    private List<ListOfFemiAgreementLineItemAsset> listOfFemiAgreementLineItemAsset = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-   private final static long serialVersionUID = 7862339401013262127L;
+    @JsonProperty("Id")
+    public String getId() {
+        return id;
+    }
 
-   @JsonProperty("Id")
-   public String getId()
-   {
-      return id;
-   }
+    @JsonProperty("Id")
+    public void setId(String id) {
+        this.id = id;
+    }
 
-   @JsonProperty("Id")
-   public void setId(String id)
-   {
-      this.id = id;
-   }
+    @JsonProperty("Expose")
+    public String getExpose() {
+        return expose;
+    }
 
-   @JsonProperty("Expose")
-   public String getExpose()
-   {
-      return expose;
-   }
+    @JsonProperty("Expose")
+    public void setExpose(String expose) {
+        this.expose = expose;
+    }
 
-   @JsonProperty("Expose")
-   public void setExpose(String expose)
-   {
-      this.expose = expose;
-   }
+    @JsonProperty("MaximumServices")
+    public String getMaximumServices() {
+        return maximumServices;
+    }
 
-   @JsonProperty("MaximumServices")
-   public String getMaximumServices()
-   {
-      return maximumServices;
-   }
+    @JsonProperty("MaximumServices")
+    public void setMaximumServices(String maximumServices) {
+        this.maximumServices = maximumServices;
+    }
 
-   @JsonProperty("MaximumServices")
-   public void setMaximumServices(String maximumServices)
-   {
-      this.maximumServices = maximumServices;
-   }
+    @JsonProperty("Name4")
+    public String getName4() {
+        return name4;
+    }
 
-   @JsonProperty("Name4")
-   public String getName4()
-   {
-      return name4;
-   }
+    @JsonProperty("Name4")
+    public void setName4(String name4) {
+        this.name4 = name4;
+    }
 
-   @JsonProperty("Name4")
-   public void setName4(String name4)
-   {
-      this.name4 = name4;
-   }
+    @JsonProperty("QualificationDate2")
+    public String getQualificationDate2() {
+        return qualificationDate2;
+    }
 
-   @JsonProperty("SumServicesUsage")
-   public String getSumServicesUsage()
-   {
-      return sumServicesUsage;
-   }
+    @JsonProperty("QualificationDate2")
+    public void setQualificationDate2(String qualificationDate2) {
+        this.qualificationDate2 = qualificationDate2;
+    }
 
-   @JsonProperty("SumServicesUsage")
-   public void setSumServicesUsage(String sumServicesUsage)
-   {
-      this.sumServicesUsage = sumServicesUsage;
-   }
+    @JsonProperty("SumServicesUsage")
+    public String getSumServicesUsage() {
+        return sumServicesUsage;
+    }
 
-@JsonProperty("listOfFemiAgreementLineItemAsset")
-  public ListOfFemiAgreementLineItemAsset[] getListOfFemiAgreementLineItemAsset ()
-    {
+    @JsonProperty("SumServicesUsage")
+    public void setSumServicesUsage(String sumServicesUsage) {
+        this.sumServicesUsage = sumServicesUsage;
+    }
+
+    @JsonProperty("ListOfFemiAgreementLineItemAsset")
+    public List<ListOfFemiAgreementLineItemAsset> getListOfFemiAgreementLineItemAsset() {
         return listOfFemiAgreementLineItemAsset;
     }
 
-@JsonProperty("listOfFemiAgreementLineItemAsset")
-public void setListOfFemiAgreementLineItemAsset (ListOfFemiAgreementLineItemAsset[] listOfFemiAgreementLineItemAsset)
-    {
+    @JsonProperty("ListOfFemiAgreementLineItemAsset")
+    public void setListOfFemiAgreementLineItemAsset(List<ListOfFemiAgreementLineItemAsset> listOfFemiAgreementLineItemAsset) {
         this.listOfFemiAgreementLineItemAsset = listOfFemiAgreementLineItemAsset;
     }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+	public FemiAgreementEntitlement(String id, String expose, String maximumServices, String name4,
+			String qualificationDate2, String sumServicesUsage,
+			List<ListOfFemiAgreementLineItemAsset> listOfFemiAgreementLineItemAsset,
+			Map<String, Object> additionalProperties) {
+		super();
+		this.id = id;
+		this.expose = expose;
+		this.maximumServices = maximumServices;
+		this.name4 = name4;
+		this.qualificationDate2 = qualificationDate2;
+		this.sumServicesUsage = sumServicesUsage;
+		this.listOfFemiAgreementLineItemAsset = listOfFemiAgreementLineItemAsset;
+		this.additionalProperties = additionalProperties;
+	}
+
     
-
-   public java.util.Date getQualificationDate2()
-   {
-      return this.qualificationDate2;
-   }
-
-   public void setQualificationDate2(java.util.Date qualificationDate2)
-   {
-      this.qualificationDate2 = qualificationDate2;
-   }
-
-   public FemiAgreementEntitlement()
-   {
-   }
-
-     public FemiAgreementEntitlement(
-         java.lang.String id,
-         java.lang.String expose,
-         java.lang.String maximumServices,
-         java.lang.String name4,
-         java.util.Date qualificationDate2,
-         java.lang.String sumServicesUsage,
-         ListOfFemiAgreementLineItemAsset[] listOfFemiAgreementLineItemAsset)
-   {
-      this.id = id;
-      this.expose = expose;
-      this.maximumServices = maximumServices;
-      this.name4 = name4;
-      this.qualificationDate2 = qualificationDate2;
-      this.sumServicesUsage = sumServicesUsage;
-      this.listOfFemiAgreementLineItemAsset = listOfFemiAgreementLineItemAsset;
-   }
-
 }
