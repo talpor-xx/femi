89855c82afe8c697baa2cd2a6bed44714885104f
brms <>
2018-10-07 20:04:21 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementLineItemAsset.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementLineItemAsset.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/FemiAgreementLineItemAsset.java
@@ -2,6 +2,7 @@
 package com.femi.femi_poc.model.json;
 
 import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
