e71973a020cd8f3b3ca78fc9b8ee11a54b99aabb
brms <>
2018-10-08 18:59:23 +0300

    {/femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFEMIFSAgreementSubItem.java}

--- femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFEMIFSAgreementSubItem.java
+++ femi_poc/src/main/java/com/femi/femi_poc/model/json/ListOfFEMIFSAgreementSubItem.java
@@ -1,31 +1,121 @@
-
 package com.femi.femi_poc.model.json;
 
-import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-    "FEMIFSAgreementSubItem"
+    "Id",
+    "ParentAgreementItemId",
+    "ProductId",
+    "LineNumber",
+    "Product",
+    "Write-InProduct",
+    "LineNumber2"
 })
-public class ListOfFEMIFSAgreementSubItem implements Serializable
-{
+public class ListOfFEMIFSAgreementSubItem {
+    @JsonProperty("Id")
+    private String id;
+    @JsonProperty("ParentAgreementItemId")
+    private String parentAgreementItemId;
+    @JsonProperty("ProductId")
+    private String productId;
+    @JsonProperty("LineNumber")
+    private String lineNumber;
+    @JsonProperty("Product")
+    private String product;
+    @JsonProperty("Write-InProduct")
+    private String writeInProduct;
+    @JsonProperty("LineNumber2")
+    private String lineNumber2;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-    @JsonProperty("FEMIFSAgreementSubItem")
-    private List<FEMIFSAgreementSubItem> fEMIFSAgreementSubItem = null;
-    private final static long serialVersionUID = 6435530656760871593L;
-
-    @JsonProperty("FEMIFSAgreementSubItem")
-    public List<FEMIFSAgreementSubItem> getFEMIFSAgreementSubItem() {
-        return fEMIFSAgreementSubItem;
+    @JsonProperty("Id")
+    public String getId() {
+        return id;
     }
 
-    @JsonProperty("FEMIFSAgreementSubItem")
-    public void setFEMIFSAgreementSubItem(List<FEMIFSAgreementSubItem> fEMIFSAgreementSubItem) {
-        this.fEMIFSAgreementSubItem = fEMIFSAgreementSubItem;
+    @JsonProperty("Id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("ParentAgreementItemId")
+    public String getParentAgreementItemId() {
+        return parentAgreementItemId;
+    }
+
+    @JsonProperty("ParentAgreementItemId")
+    public void setParentAgreementItemId(String parentAgreementItemId) {
+        this.parentAgreementItemId = parentAgreementItemId;
+    }
+
+    @JsonProperty("ProductId")
+    public String getProductId() {
+        return productId;
+    }
+
+    @JsonProperty("ProductId")
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    @JsonProperty("LineNumber")
+    public String getLineNumber() {
+        return lineNumber;
+    }
+
+    @JsonProperty("LineNumber")
+    public void setLineNumber(String lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    @JsonProperty("Product")
+    public String getProduct() {
+        return product;
+    }
+
+    @JsonProperty("Product")
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    @JsonProperty("Write-InProduct")
+    public String getWriteInProduct() {
+        return writeInProduct;
+    }
+
+    @JsonProperty("Write-InProduct")
+    public void setWriteInProduct(String writeInProduct) {
+        this.writeInProduct = writeInProduct;
+    }
+
+    @JsonProperty("LineNumber2")
+    public String getLineNumber2() {
+        return lineNumber2;
+    }
+
+    @JsonProperty("LineNumber2")
+    public void setLineNumber2(String lineNumber2) {
+        this.lineNumber2 = lineNumber2;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
     }
 
 }
